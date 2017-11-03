22ad6b1beebf790c6e09921f6f440a144c836450
Giulio Zhou <giuliozhou8@gmail.com>
2017-11-03 18:28:39 +0000

    Working basic Tensorflow image process example

---
+++ projects/kernels/tf_op.py
@@ -0,0 +1,23 @@
+import scannerpy
+import tensorflow as tf
+
+class TfOpKernel(scannerpy.Kernel):
+    def __init__(self, config, protobufs):
+        self.protobufs = protobufs
+        # args = protobufs.TfOpArgs()
+        # args.ParseFromString(config)
+        # model_fn = get_model_fn(args.model_name)
+        self.sess = tf.Session()
+        with tf.device('/gpu:0'):
+            self.image = tf.placeholder('uint8', [400, 600, 3], name="input_image")
+            self.output = tf.scalar_mul(2, self.image)
+
+    def close(self):
+        pass
+
+    def execute(self, input_columns):
+        scaled_input_columns = self.sess.run(self.output,
+                                             {self.image: input_columns[0]})
+        return input_columns
+
+KERNEL = TfOpKernel
