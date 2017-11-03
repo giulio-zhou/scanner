3bc073c2573c3d3c2c6eebe8404153da9e763331
Giulio Zhou <giuliozhou8@gmail.com>
2017-11-03 18:28:39 +0000

    Working basic Tensorflow image process example

---
+++ projects/kernels/py_input.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scannerpy
+
+class PyInputKernel(scannerpy.Kernel):
+    def __init__(self, config, protobufs):
+        self.protobufs = protobufs
+
+    def close(self):
+        pass
+
+    def execute(self, input_columns):
+        return input_columns
+
+KERNEL = PyInputKernel
--- projects/run_tf_hashes.py
+++ projects/run_tf_hashes.py
@@ -6,20 +6,28 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 video_path = sys.argv[1]
 
 with Database() as db:
-    db.register_op('Test', [('frame', ColumnType.Video)], ['output'])
-    db.register_python_kernel('Test', DeviceType.GPU,
-                              script_dir + '/models.py')
+    db.register_op('PyInput', [('input_frame', ColumnType.Video)], ['frame'])
+    db.register_python_kernel('PyInput', DeviceType.CPU,
+                              script_dir + '/kernels/py_input.py')
+    db.register_op('TfOp', [('input_frame', ColumnType.Video)], ['frame'])
+    db.register_python_kernel('TfOp', DeviceType.CPU,
+                              script_dir + '/kernels/tf_op.py')
 
     load_video_from_scratch = True
     if not db.has_table('target_video') or load_video_from_scratch:
         db.ingest_videos([('target_video', video_path)], force=True)
 
     frame = db.ops.FrameInput()
-    tf_frame = db.ops.Test(
-        frame = frame,
-        device=DeviceType.GPU
+    py_input = db.ops.PyInput(
+        input_frame = frame,
+        device=DeviceType.CPU
     )
-    output_op = db.ops.Output(columns=[tf_frame])
+    tf_out = db.ops.TfOp(
+        input_frame = py_input,
+        device=DeviceType.CPU
+    )
+        
+    output_op = db.ops.Output(columns=[tf_out])
 
     job = Job(
         op_args={
