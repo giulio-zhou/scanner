0f227fdf78f1536013403d12fd20d679de37d3ec
Giulio Zhou <giuliozhou8@gmail.com>
2018-01-22 22:29:27 +0000

    Move neural network code into separate repo

--- projects/kernels/caffe_op.py
+++ projects/kernels/caffe_op.py
@@ -1,41 +1,17 @@
-import caffe
-import numpy as np
 import scannerpy
-import tensorflow as tf
-from caffe_models import get_model_fn
+from run_nn_models.model_runners import CaffeModelRunner
 
 class CaffeOpKernel(scannerpy.Kernel):
     def __init__(self, config, protobufs):
         self.protobufs = protobufs
         args = protobufs.TfOpArgs()
         args.ParseFromString(config)
-
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.2)
-        self.sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-        self.batch_size = args.batch_size
-        self.model_dict = get_model_fn(args.model_name,
-                                       batch_size=self.batch_size)
-
-        caffe.set_mode_gpu()
-        caffe.set_device(0)
-
-        self.image_width, self.image_height = self.model_dict['input_dims']
-        self.model_path = self.model_dict['model_prototxt_path']
-        self.weights_path = self.model_dict['model_weights_path']
-
-        self.model = caffe.Net(self.model_path, self.weights_path, caffe.TEST)
-        self.model.blobs['data'].reshape(
-            self.batch_size, 3, self.image_height, self.image_width)
+        self.model_runner = CaffeModelRunner(args.model_name, args.batch_size)
 
     def close(self):
         pass
 
     def execute(self, input_columns):
-        inputs = \
-            self.model_dict['input_preprocess_fn'](self.sess, input_columns)
-        outputs = self.model_dict['inference_fn'](self.model, inputs)
-        post_processed_outputs = self.model_dict['post_processing_fn'](
-            input_columns, outputs, self.sess)
-        return post_processed_outputs
+        return self.model_runner.execute(input_columns)
 
 KERNEL = CaffeOpKernel
