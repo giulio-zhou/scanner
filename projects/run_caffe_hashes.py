76b420a98a7b33638e90984cde922d5a8a33a72a
Giulio Zhou <giuliozhou8@gmail.com>
2018-02-23 20:47:17 +0000

    h5py output for scanner stuff

--- projects/run_caffe_hashes.py
+++ projects/run_caffe_hashes.py
@@ -1,4 +1,5 @@
 from scannerpy import Database, DeviceType, Job, BulkJob, ColumnType
+import h5py
 import numpy as np
 import os
 import sys
@@ -37,7 +38,8 @@ with Database() as db:
     job = Job(
         op_args={
             frame: db.table('target_video').column('frame'),
-            sampled_frames: db.sampler.gather([i for i in range(0, 20, 1)]),
+            # sampled_frames: db.sampler.gather([i for i in range(0, 20, 1)]),
+            sampled_frames: db.sampler.all(),
             output_op: 'hashes'
         }
     )
@@ -50,11 +52,17 @@ with Database() as db:
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     # Process outputs into numpy array.
-    feature_vecs = output.column('feature_vector').load()
-    feature_vec_npy = np.array([np.loads(v[1]) for v in feature_vecs])
-    print(feature_vec_npy, feature_vec_npy.shape)
-    labels = np.arange(len(feature_vec_npy))
+    size = output.column('feature_vector').size()
+    dims = np.loads(next(output.column('feature_vector').load())[1]).shape
+    hdf5_file = h5py.File('%s/feature_vectors.h5' % output_dir, mode='w')
+    hdf5_file.create_dataset('data', [size] + list(dims), 'f4')
+    for i, val in output.column('feature_vector').load():
+        hdf5_file['data'][i] = val
+    # feature_vecs = output.column('feature_vector').load()
+    # feature_vec_npy = np.array([np.loads(v[1]) for v in feature_vecs])
+    # print(feature_vec_npy, feature_vec_npy.shape)
+    labels = np.arange(size)
     labels = labels.reshape(-1, 1)
     # Write numpy arrays to output directory.
-    np.save('%s/feature_vectors.npy' % output_dir, feature_vec_npy)
+    # np.save('%s/feature_vectors.npy' % output_dir, feature_vec_npy)
     np.save('%s/labels.npy' % output_dir, labels)
